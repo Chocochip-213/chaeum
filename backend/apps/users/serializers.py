48ea932771c9051c8b2967282d61ade4e4ce8b0b
SeoJumee <jumee.frontdev@gmail.com>
2025-12-21 23:28:12 +0900

    Feat: 비밀번호 재설정, 로그아웃, 회원탈퇴 기능 추가

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_str
 
 User = get_user_model()
 
@@ -17,3 +20,43 @@ class UserSerializer(serializers.ModelSerializer):
             campus=validated_data['campus']
         )
         return user
+    
+class PasswordChangeSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("현재 비밀번호가 일치하지 않습니다.")
+        return value
+
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate_email(self, value):
+        if not User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("해당 이메일로 가입된 계정이 없습니다.")
+        return value
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    new_password = serializers.CharField(write_only=True, min_length=8)
+    uidb64 = serializers.CharField()
+    token = serializers.CharField()
+
+    def validate(self, attrs):
+        try:
+            uid = force_str(urlsafe_base64_decode(attrs['uidb64']))
+            self.user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            raise serializers.ValidationError("유효하지 않은 유저 ID입니다.")
+
+        if not default_token_generator.check_token(self.user, attrs['token']):
+            raise serializers.ValidationError("유효하지 않거나 만료된 토큰입니다.")
+
+        return attrs
+
+    def save(self):
+        self.user.set_password(self.validated_data['new_password'])
+        self.user.save()
+        return self.user
