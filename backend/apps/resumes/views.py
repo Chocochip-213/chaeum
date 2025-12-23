60f6e62a45d6dd1b2638677ade94ac7dceec3232
chip718828 <chip718828@naver.com>
2025-12-23 10:43:09 +0900

    Feat: 이력서 업로드 및 조회 API 구현.

--- backend/apps/resumes/views.py
+++ backend/apps/resumes/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, parsers, status
+from rest_framework.response import Response
+from .models import Resume
+from .serializers import ResumeSerializer
+from apps.analysis.utils.parsers import PDFParser # 이력서 파싱 스크립트임.
 
-# Create your views here.
+
+class ResumeViewSet(viewsets.ModelViewSet):
+    serializer_class = ResumeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    parser_classes = [parsers.MultiPartParser, parsers.FormParser]
+
+    def get_queryset(self):
+        return Resume.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        uploaded_file = self.request.FILES.get('file')
+        if not uploaded_file:
+            return
+
+        parser = PDFParser()
+        parsed_text = ""
+
+        try:
+            file_bytes = uploaded_file.read()
+            parsed_text = parser.parse_stream(file_bytes)
+        except Exception as e:
+            print(f"Parsing Error: {e}")
+
+        serializer.save(
+            user=self.request.user,
+            file_name=uploaded_file.name,
+            parsed_content=parsed_text,
+            status='SUCCESS'
+        )
