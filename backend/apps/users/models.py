98b8c8815e1c85b2dab356ab38b5f85a183d9141
SeoJumee <jumee.frontdev@gmail.com>
2025-12-23 09:40:08 +0900

    Chore: User 모델 address 컬럼 생성

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -22,6 +22,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True, verbose_name="가입일")
+    address = models.TextField(verbose_name="주소", blank=True, null=True)
 
     objects = UserManager()
 
